b2b0f85afe898e90f9314696a63c7c6c7cb30516
Konark Bhad <90366430+konarkbhad@users.noreply.github.com>
2023-08-05 22:42:24 -0700

    Time: 50 ms (64.64%), Space: 16.2 MB (98.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        last = m + n - 1
+        last = m + n - 1 # last index in nums1
         m -= 1
         n -= 1
         
+        # fill in reverse order
         while m >= 0 and n >= 0:
             if nums1[m] < nums2[n]:
                 nums1[last] = nums2[n]
@@ -16,6 +17,8 @@ class Solution:
                 m -= 1
             last -= 1
         
+        # fill nums1 with leftovers from nums2
+        # will have leftovers if nums2 have small elements than nums1 nums1 = [1, 0] nums2 = [0]
         while n >= 0:
             nums1[last] = nums2[n]
             last -= 1

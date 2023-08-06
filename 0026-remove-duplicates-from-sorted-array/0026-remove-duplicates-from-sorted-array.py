c34c3561a01976f0cf0cdfd5154c022287015c01
Konark Bhad <90366430+konarkbhad@users.noreply.github.com>
2023-08-06 15:19:45 -0700

    Time: 83 ms (94.07%), Space: 18 MB (56.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,5 +1,16 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        '''
+        left ptr -> to keep the element that the right ptr 
+        is pointing to
+        
+        right ptr -> will check with preceding if equal
+        and when not equal means that is unique and will keep
+        that on left ptr
+        
+        first element will always be unique
+        so starting from 1 and not 0
+        '''
         left = 1
 
         for right in range(1, len(nums)):
